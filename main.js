802752e02c7d287596d10237bab1375351591299
SOUMYA SAMANTA <soumyasamanta2004@gmail.com>
2025-07-08 04:02:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".text", {
+        strings: ["Web Designer", "Frontend Developer", "UI/UX Designer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
